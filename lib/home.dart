7fab7975046a26b81f36c09446636707032ce263
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-10-04 18:53:33 +0530

    refactoring+responsiveness

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,7 @@
-import 'dart:math' as math;
 import 'dart:ui';
 
+import 'package:among_us_app/bottomPanel.dart';
 import 'package:among_us_app/generateButton.dart';
-import 'package:among_us_app/starPainter.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:share/share.dart';
@@ -85,7 +84,7 @@ class _HomePageState extends State<HomePage> {
         minHeight: 125,
         maxHeight: 400,
         parallaxEnabled: true,
-        parallaxOffset: 0.8,
+        parallaxOffset: 0.7,
         borderRadius: BorderRadius.only(
             topLeft: Radius.circular(25), topRight: Radius.circular(25)),
         collapsed: Stack(
@@ -120,11 +119,11 @@ class _HomePageState extends State<HomePage> {
             ),
             Image.asset(
               "assets/images/BG/BG${Provider.of<globals.RVProvider>(context).bgVariable}-01.png",
-              height: 200,
+              height: MediaQuery.of(context).size.width * 0.6,
             ),
             Image.asset(
               "assets/images/CH/CH${Provider.of<globals.RVProvider>(context).colorVariable}-01.png",
-              height: 200,
+              height: MediaQuery.of(context).size.width * 0.6,
             )
           ],
         ),
@@ -132,104 +131,6 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: GenerateButton(),
-    );
-  }
-}
-
-class GenerateBottomPanel extends StatelessWidget {
-  const GenerateBottomPanel({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(25), topRight: Radius.circular(25)),
-        border: Border.all(color: Colors.white, width: 4),
-        color: Colors.black,
-      ),
-      child: SizedBox.expand(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                    icon: Icon(
-                      Icons.chevron_left,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      Provider.of<globals.RVProvider>(context, listen: false)
-                          .shiftBGVariable(false);
-                    }),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  child: Text(
-                    "BG${Provider.of<globals.RVProvider>(context).bgVariable}-01.png",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                IconButton(
-                    icon: Icon(
-                      Icons.chevron_right,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      Provider.of<globals.RVProvider>(context, listen: false)
-                          .shiftBGVariable(true);
-                    }),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                    icon: Icon(
-                      Icons.chevron_left,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      Provider.of<globals.RVProvider>(context, listen: false)
-                          .shiftColorVariable(false);
-                    }),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  child: Text(
-                    "CH${Provider.of<globals.RVProvider>(context).colorVariable}-01.png",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                IconButton(
-                    icon: Icon(
-                      Icons.chevron_right,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      Provider.of<globals.RVProvider>(context, listen: false)
-                          .shiftColorVariable(true);
-                    }),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Stars extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: StarPainter(particles: 30),
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-      ),
     );
   }
 }
